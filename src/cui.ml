6e2f072c9042d48d7652e4f4f16f4d16f97e7e40
austenppm <austenppm12345@gmail.com>
2023-07-14 11:07:39 +0900

    3.3.3

--- src/cui.ml
+++ src/cui.ml
@@ -16,7 +16,21 @@ with
     print_string ("Error: " ^ e ^ "\n");
     read_eval_print env
 
+let read_eval_print_from_file filename env =
+  let in_channel = open_in filename in
+  try
+    let program = really_input_string in_channel (in_channel_length in_channel) in
+    let decl = Parser.toplevel Lexer.main (Lexing.from_string program) in
+    let (id, newenv, v) = Eval.eval_decl env decl in
+    Printf.printf "val %s = " id;
+    pp_val v;
+    print_newline();
+    newenv
+  with e ->
+    close_in in_channel;
+    raise e
 
+    
 let initial_env =
   Environment.extend "iv" (IntV 4)
     (Environment.extend "iii" (IntV 3)
